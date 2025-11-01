11220eff380f19445778af09ab8a9d298f247b5f
marcelpeterson <marcel.peterson@binus.ac.id>
2025-11-01 10:58:06 +0700

    Update listing.blade.php

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -406,7 +406,7 @@
                             <p class="text-xs text-gray-500 truncate">{{ $item->user->email }}</p>
                             @if($item->user->location)
                                 <div class="flex items-center mt-1">
-                                    <svg class="w-3 h-3 text-gray-500 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <svg class="w-3 h-3 text-gray-500 mr-1 max-md:mb-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
                                     </svg>
